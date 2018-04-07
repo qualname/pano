1b5230426a32005a3f9791ad941cc9ebc2ed44f8
qualname <mhegyi@pm.me>
2018-04-07 19:46:43 +0200

    added max spanning tree search

--- utils.hpp
+++ utils.hpp
@@ -11,9 +11,28 @@
 namespace utils {
 
 
+struct CmpWeightDesc {
+    bool operator()(const std::tuple<int, int, double> & t1,
+                    const std::tuple<int, int, double> & t2)
+    {
+        return std::get<2>(t1) >= std::get<2>(t2);
+    }
+};
+
+
+struct Increment {
+    Increment(std::map<int, int> & distances_) : distances(distances_) {}
+    void operator()(const cv::detail::GraphEdge & edge) {
+        distances[edge.to] = distances[edge.from] + 1;
+    }
+
+    std::map<int, int> & distances;
+};
+
 class AdjacencyMatrix {
 public:
     AdjacencyMatrix(const std::vector<std::vector<cv::detail::MatchesInfo>> & matrix, double threshold)
+    : components(static_cast<int>(matrix.size()))
     {
         const auto num_of_vertices = static_cast<int>(matrix.size());
         adj_matrix.resize(num_of_vertices);
@@ -43,7 +62,51 @@ public:
         return components;
     }
 
-    std::pair<cv::detail::Graph, int> find_max_span_tree(const std::set<int> & vertices);
+    std::pair<cv::detail::Graph, int> find_max_span_tree(const std::set<int>     & vertices,
+                                                               cv::detail::Graph & span_tree)
+    {
+        const auto num_of_vertices = static_cast<int>(vertices.size());
+
+        std::set<std::tuple<int, int, double>, CmpWeightDesc> edges;
+        for (const auto v1 : vertices)
+            for (const auto v2 : vertices)
+                if (v1 < v2)
+                    edges.insert(std::make_tuple(v1, v2, adj_matrix[v1][v2]));
+
+        int from, to;
+        double weight;
+        std::map<int, int> power;
+        for (const auto & edge : edges) {
+            std::tie(from, to, weight) = edge;
+            int from_comp_idx = components.findSetByElem(from);
+            int   to_comp_idx = components.findSetByElem(to);
+            if (from_comp_idx == to_comp_idx) continue;
+
+            components.mergeSets(from_comp_idx, to_comp_idx);
+
+            span_tree.addEdge(from, to, static_cast<float>(weight));
+            ++power[from];
+            span_tree.addEdge(to, from, static_cast<float>(weight));
+            ++power[to];
+        }
+
+        std::vector<int> leafs;
+        for (const auto & [v, pwr] : power)
+            if (pwr == 1) leafs.push_back(v);
+
+        std::map<int, int> max_distances;
+        for (const auto leaf : leafs) {
+            std::map<int, int> curr_distance;
+            span_tree.walkBreadthFirst(leaf, Increment(curr_distance));
+            for (const auto v : vertices)
+                max_distances[v] = std::max(max_distances[v], curr_distance[v]);
+        }
+
+        auto max_dist = std::max_element(max_distances.cbegin(), max_distances.cend(), [](const auto & p1, const auto & p2) {
+            return p1.second < p2.second; });
+
+        return std::make_pair(span_tree, max_dist->first);
+    }
 
 private:
 
@@ -82,6 +145,7 @@ private:
     }
 
     std::vector<std::vector<double>> adj_matrix;
+    cv::detail::DisjointSets components;
 };
 
 
