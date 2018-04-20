2d37e2a660ae88736924854596450772fe3ca980
qualname <mhegyi@pm.me>
2018-04-20 15:54:11 +0200

    added formulas for spherical warper

--- warper.hpp
+++ warper.hpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <vector>
 
 #include "opencv2/opencv.hpp"
@@ -5,7 +6,26 @@
 namespace warper {
 
 
-class SphericalMap;
+class SphericalMap {
+public:
+    SphericalMap(float s_) : s(s_) {} 
+
+    void map_coord(const h_point & p)
+    {
+        float x_ = s * std::atan2(p.x, p.z);
+        float y_ = s * std::atan2(p.y, std::hypot(p.x, p.z));
+    }
+
+    void inv_map_coord(const point & p_)
+    {
+        float x = std::tan(p_.x / s);
+        float y = std::tan(p_.y / s) / std::cos(p_.x / s);
+        float z = 1.f;
+    }
+
+private:
+    float s;
+};
 
 
 float get_radius(const std::vector<cv::detail::CameraParams> & cams)
